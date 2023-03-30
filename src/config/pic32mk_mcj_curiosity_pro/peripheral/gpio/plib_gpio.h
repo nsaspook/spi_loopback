82441934c2d41e3fc37edbcf49ba38c63fed577c
nsaspook <nsaspook@nsaspook.com>
2023-03-30 16:31:49 -0700

    up

---
+++ src/config/pic32mk_mcj_curiosity_pro/peripheral/gpio/plib_gpio.h
@@ -0,0 +1,397 @@
+/*******************************************************************************
+  GPIO PLIB
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    plib_gpio.h UUUUUUUUU
+
+  Summary:
+    GPIO PLIB Header File
+
+  Description:
+    This library provides an interface to control and interact with Parallel
+    Input/Output controller (GPIO) module.
+
+*******************************************************************************/
+
+/*******************************************************************************
+* Copyright (C) 2019 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+
+#ifndef PLIB_GPIO_H
+#define PLIB_GPIO_H
+
+#include <device.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+
+// DOM-IGNORE-BEGIN
+#ifdef __cplusplus  // Provide C++ Compatibility
+
+    extern "C" {
+
+#endif
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Data types and constants
+// *****************************************************************************
+// *****************************************************************************
+
+
+/*** Macros for MOSI1 pin ***/
+#define MOSI1_Get()               ((PORTA >> 0) & 0x1U)
+#define MOSI1_PIN                  GPIO_PIN_RA0
+
+/*** Macros for SS_CS pin ***/
+#define SS_CS_Set()               (LATBSET = (1U<<2))
+#define SS_CS_Clear()             (LATBCLR = (1U<<2))
+#define SS_CS_Toggle()            (LATBINV= (1U<<2))
+#define SS_CS_OutputEnable()      (TRISBCLR = (1U<<2))
+#define SS_CS_InputEnable()       (TRISBSET = (1U<<2))
+#define SS_CS_Get()               ((PORTB >> 2) & 0x1U)
+#define SS_CS_PIN                  GPIO_PIN_RB2
+
+/*** Macros for MISO1 pin ***/
+#define MISO1_Get()               ((PORTC >> 0) & 0x1U)
+#define MISO1_PIN                  GPIO_PIN_RC0
+
+/*** Macros for RS pin ***/
+#define RS_Set()               (LATCSET = (1U<<1))
+#define RS_Clear()             (LATCCLR = (1U<<1))
+#define RS_Toggle()            (LATCINV= (1U<<1))
+#define RS_OutputEnable()      (TRISCCLR = (1U<<1))
+#define RS_InputEnable()       (TRISCSET = (1U<<1))
+#define RS_Get()               ((PORTC >> 1) & 0x1U)
+#define RS_PIN                  GPIO_PIN_RC1
+
+/*** Macros for CSB pin ***/
+#define CSB_Set()               (LATCSET = (1U<<2))
+#define CSB_Clear()             (LATCCLR = (1U<<2))
+#define CSB_Toggle()            (LATCINV= (1U<<2))
+#define CSB_OutputEnable()      (TRISCCLR = (1U<<2))
+#define CSB_InputEnable()       (TRISCSET = (1U<<2))
+#define CSB_Get()               ((PORTC >> 2) & 0x1U)
+#define CSB_PIN                  GPIO_PIN_RC2
+
+/*** Macros for SPI2_REC_DATA pin ***/
+#define SPI2_REC_DATA_Set()               (LATESET = (1U<<12))
+#define SPI2_REC_DATA_Clear()             (LATECLR = (1U<<12))
+#define SPI2_REC_DATA_Toggle()            (LATEINV= (1U<<12))
+#define SPI2_REC_DATA_OutputEnable()      (TRISECLR = (1U<<12))
+#define SPI2_REC_DATA_InputEnable()       (TRISESET = (1U<<12))
+#define SPI2_REC_DATA_Get()               ((PORTE >> 12) & 0x1U)
+#define SPI2_REC_DATA_PIN                  GPIO_PIN_RE12
+
+/*** Macros for csPin_m pin ***/
+#define csPin_m_Set()               (LATESET = (1U<<15))
+#define csPin_m_Clear()             (LATECLR = (1U<<15))
+#define csPin_m_Toggle()            (LATEINV= (1U<<15))
+#define csPin_m_OutputEnable()      (TRISECLR = (1U<<15))
+#define csPin_m_InputEnable()       (TRISESET = (1U<<15))
+#define csPin_m_Get()               ((PORTE >> 15) & 0x1U)
+#define csPin_m_PIN                  GPIO_PIN_RE15
+
+/*** Macros for dtog pin ***/
+#define dtog_Set()               (LATASET = (1U<<4))
+#define dtog_Clear()             (LATACLR = (1U<<4))
+#define dtog_Toggle()            (LATAINV= (1U<<4))
+#define dtog_OutputEnable()      (TRISACLR = (1U<<4))
+#define dtog_InputEnable()       (TRISASET = (1U<<4))
+#define dtog_Get()               ((PORTA >> 4) & 0x1U)
+#define dtog_PIN                  GPIO_PIN_RA4
+
+/*** Macros for csPin_ag pin ***/
+#define csPin_ag_Set()               (LATESET = (1U<<1))
+#define csPin_ag_Clear()             (LATECLR = (1U<<1))
+#define csPin_ag_Toggle()            (LATEINV= (1U<<1))
+#define csPin_ag_OutputEnable()      (TRISECLR = (1U<<1))
+#define csPin_ag_InputEnable()       (TRISESET = (1U<<1))
+#define csPin_ag_Get()               ((PORTE >> 1) & 0x1U)
+#define csPin_ag_PIN                  GPIO_PIN_RE1
+
+/*** Macros for DBLANK pin ***/
+#define DBLANK_Set()               (LATCSET = (1U<<10))
+#define DBLANK_Clear()             (LATCCLR = (1U<<10))
+#define DBLANK_Toggle()            (LATCINV= (1U<<10))
+#define DBLANK_OutputEnable()      (TRISCCLR = (1U<<10))
+#define DBLANK_InputEnable()       (TRISCSET = (1U<<10))
+#define DBLANK_Get()               ((PORTC >> 10) & 0x1U)
+#define DBLANK_PIN                  GPIO_PIN_RC10
+
+/*** Macros for MOSI2 pin ***/
+#define MOSI2_Get()               ((PORTC >> 7) & 0x1U)
+#define MOSI2_PIN                  GPIO_PIN_RC7
+
+/*** Macros for MISO2 pin ***/
+#define MISO2_Get()               ((PORTD >> 6) & 0x1U)
+#define MISO2_PIN                  GPIO_PIN_RD6
+
+/*** Macros for SS2_IN pin ***/
+#define SS2_IN_Get()               ((PORTB >> 10) & 0x1U)
+#define SS2_IN_PIN                  GPIO_PIN_RB10
+#define SS2_IN_InterruptEnable()   (CNENBSET = (1U<<10))
+#define SS2_IN_InterruptDisable()  (CNENBCLR = (1U<<10))
+
+/*** Macros for LED pin ***/
+#define LED_Set()               (LATASET = (1U<<10))
+#define LED_Clear()             (LATACLR = (1U<<10))
+#define LED_Toggle()            (LATAINV= (1U<<10))
+#define LED_OutputEnable()      (TRISACLR = (1U<<10))
+#define LED_InputEnable()       (TRISASET = (1U<<10))
+#define LED_Get()               ((PORTA >> 10) & 0x1U)
+#define LED_PIN                  GPIO_PIN_RA10
+
+
+// *****************************************************************************
+/* GPIO Port
+
+  Summary:
+    Identifies the available GPIO Ports.
+
+  Description:
+    This enumeration identifies the available GPIO Ports.
+
+  Remarks:
+    The caller should not rely on the specific numbers assigned to any of
+    these values as they may change from one processor to the next.
+
+    Not all ports are available on all devices.  Refer to the specific
+    device data sheet to determine which ports are supported.
+*/
+
+
+#define    GPIO_PORT_A  (0)
+#define    GPIO_PORT_B  (1)
+#define    GPIO_PORT_C  (2)
+#define    GPIO_PORT_D  (3)
+#define    GPIO_PORT_E  (4)
+#define    GPIO_PORT_F  (5)
+#define    GPIO_PORT_G  (6)
+typedef uint32_t GPIO_PORT;
+
+typedef enum
+{
+    GPIO_INTERRUPT_ON_MISMATCH,
+    GPIO_INTERRUPT_ON_RISING_EDGE,
+    GPIO_INTERRUPT_ON_FALLING_EDGE,
+    GPIO_INTERRUPT_ON_BOTH_EDGES,
+}GPIO_INTERRUPT_STYLE;
+
+// *****************************************************************************
+/* GPIO Port Pins
+
+  Summary:
+    Identifies the available GPIO port pins.
+
+  Description:
+    This enumeration identifies the available GPIO port pins.
+
+  Remarks:
+    The caller should not rely on the specific numbers assigned to any of
+    these values as they may change from one processor to the next.
+
+    Not all pins are available on all devices.  Refer to the specific
+    device data sheet to determine which pins are supported.
+*/
+
+
+#define     GPIO_PIN_RA0  (0U)
+#define     GPIO_PIN_RA1  (1U)
+#define     GPIO_PIN_RA4  (4U)
+#define     GPIO_PIN_RA7  (7U)
+#define     GPIO_PIN_RA8  (8U)
+#define     GPIO_PIN_RA10  (10U)
+#define     GPIO_PIN_RA11  (11U)
+#define     GPIO_PIN_RA12  (12U)
+#define     GPIO_PIN_RA14  (14U)
+#define     GPIO_PIN_RA15  (15U)
+#define     GPIO_PIN_RB0  (16U)
+#define     GPIO_PIN_RB1  (17U)
+#define     GPIO_PIN_RB2  (18U)
+#define     GPIO_PIN_RB3  (19U)
+#define     GPIO_PIN_RB4  (20U)
+#define     GPIO_PIN_RB5  (21U)
+#define     GPIO_PIN_RB6  (22U)
+#define     GPIO_PIN_RB7  (23U)
+#define     GPIO_PIN_RB8  (24U)
+#define     GPIO_PIN_RB9  (25U)
+#define     GPIO_PIN_RB10  (26U)
+#define     GPIO_PIN_RB11  (27U)
+#define     GPIO_PIN_RB12  (28U)
+#define     GPIO_PIN_RB13  (29U)
+#define     GPIO_PIN_RB14  (30U)
+#define     GPIO_PIN_RB15  (31U)
+#define     GPIO_PIN_RC0  (32U)
+#define     GPIO_PIN_RC1  (33U)
+#define     GPIO_PIN_RC2  (34U)
+#define     GPIO_PIN_RC6  (38U)
+#define     GPIO_PIN_RC7  (39U)
+#define     GPIO_PIN_RC8  (40U)
+#define     GPIO_PIN_RC9  (41U)
+#define     GPIO_PIN_RC10  (42U)
+#define     GPIO_PIN_RC11  (43U)
+#define     GPIO_PIN_RC12  (44U)
+#define     GPIO_PIN_RC13  (45U)
+#define     GPIO_PIN_RC15  (47U)
+#define     GPIO_PIN_RD5  (53U)
+#define     GPIO_PIN_RD6  (54U)
+#define     GPIO_PIN_RD8  (56U)
+#define     GPIO_PIN_RE0  (64U)
+#define     GPIO_PIN_RE1  (65U)
+#define     GPIO_PIN_RE12  (76U)
+#define     GPIO_PIN_RE13  (77U)
+#define     GPIO_PIN_RE14  (78U)
+#define     GPIO_PIN_RE15  (79U)
+#define     GPIO_PIN_RF0  (80U)
+#define     GPIO_PIN_RF1  (81U)
+#define     GPIO_PIN_RG6  (102U)
+#define     GPIO_PIN_RG7  (103U)
+#define     GPIO_PIN_RG8  (104U)
+#define     GPIO_PIN_RG9  (105U)
+
+    /* This element should not be used in any of the GPIO APIs.
+       It will be used by other modules or application to denote that none of the GPIO Pin is used */
+#define    GPIO_PIN_NONE   (-1)
+
+typedef uint32_t GPIO_PIN;
+
+typedef  void (*GPIO_PIN_CALLBACK) ( GPIO_PIN pin, uintptr_t context);
+
+void GPIO_Initialize(void);
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: GPIO Functions which operates on multiple pins of a port
+// *****************************************************************************
+// *****************************************************************************
+
+uint32_t GPIO_PortRead(GPIO_PORT port);
+
+void GPIO_PortWrite(GPIO_PORT port, uint32_t mask, uint32_t value);
+
+uint32_t GPIO_PortLatchRead ( GPIO_PORT port );
+
+void GPIO_PortSet(GPIO_PORT port, uint32_t mask);
+
+void GPIO_PortClear(GPIO_PORT port, uint32_t mask);
+
+void GPIO_PortToggle(GPIO_PORT port, uint32_t mask);
+
+void GPIO_PortInputEnable(GPIO_PORT port, uint32_t mask);
+
+void GPIO_PortOutputEnable(GPIO_PORT port, uint32_t mask);
+
+void GPIO_PortInterruptEnable(GPIO_PORT port, uint32_t mask);
+
+void GPIO_PortInterruptDisable(GPIO_PORT port, uint32_t mask);
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Local Data types and Prototypes
+// *****************************************************************************
+// *****************************************************************************
+
+typedef struct {
+
+    /* target pin */
+    GPIO_PIN                 pin;
+
+    /* Callback for event on target pin*/
+    GPIO_PIN_CALLBACK        callback;
+
+    /* Callback Context */
+    uintptr_t               context;
+
+} GPIO_PIN_CALLBACK_OBJ;
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: GPIO Functions which operates on one pin at a time
+// *****************************************************************************
+// *****************************************************************************
+
+static inline void GPIO_PinWrite(GPIO_PIN pin, bool value)
+{
+	 uint32_t xvalue = (uint32_t)value;
+    GPIO_PortWrite((pin>>4U), (uint32_t)(0x1U) << (pin & 0xFU), (xvalue) << (pin & 0xFU));
+}
+
+static inline bool GPIO_PinRead(GPIO_PIN pin)
+{
+    return ((((GPIO_PortRead((GPIO_PORT)(pin>>4U))) >> (pin & 0xFU)) & 0x1U) != 0U);
+}
+
+static inline bool GPIO_PinLatchRead(GPIO_PIN pin)
+{
+    return (((GPIO_PortLatchRead((GPIO_PORT)(pin>>4U)) >> (pin & 0xFU)) & 0x1U) != 0U);
+}
+
+static inline void GPIO_PinToggle(GPIO_PIN pin)
+{
+    GPIO_PortToggle((pin>>4U), (uint32_t)0x1U << (pin & 0xFU));
+}
+
+static inline void GPIO_PinSet(GPIO_PIN pin)
+{
+    GPIO_PortSet((pin>>4U), (uint32_t)0x1U << (pin & 0xFU));
+}
+
+static inline void GPIO_PinClear(GPIO_PIN pin)
+{
+    GPIO_PortClear((pin>>4U), (uint32_t)0x1U << (pin & 0xFU));
+}
+
+static inline void GPIO_PinInputEnable(GPIO_PIN pin)
+{
+    GPIO_PortInputEnable((pin>>4U), (uint32_t)0x1U << (pin & 0xFU));
+}
+
+static inline void GPIO_PinOutputEnable(GPIO_PIN pin)
+{
+    GPIO_PortOutputEnable((pin>>4U), (uint32_t)0x1U << (pin & 0xFU));
+}
+
+#define GPIO_PinInterruptEnable(pin)       GPIO_PinIntEnable(pin, GPIO_INTERRUPT_ON_MISMATCH)
+#define GPIO_PinInterruptDisable(pin)      GPIO_PinIntDisable(pin)
+
+void GPIO_PinIntEnable(GPIO_PIN pin, GPIO_INTERRUPT_STYLE style);
+void GPIO_PinIntDisable(GPIO_PIN pin);
+
+bool GPIO_PinInterruptCallbackRegister(
+    GPIO_PIN pin,
+    const   GPIO_PIN_CALLBACK callBack,
+    uintptr_t context
+);
+
+// DOM-IGNORE-BEGIN
+#ifdef __cplusplus  // Provide C++ Compatibility
+
+    }
+
+#endif
+// DOM-IGNORE-END
+#endif // PLIB_GPIO_H
---
+++ src/main_pic32mz_ef_sk.c
@@ -0,0 +1,107 @@
+
+#include <stddef.h>                     // Defines NULL
+#include <stdbool.h>                    // Defines true
+#include <stdlib.h>                     // Defines EXIT_FAILURE
+#include <stdio.h>
+#include <string.h>
+#include <proc/p32mk0512mcj064.h>
+#include "definitions.h"                // SYS function prototypes
+#define	BANK1		0xA000A000	// bank 1 frame buffer memory address
+#define	DMA_GAP		1		// set to 0 for SPI byte gaps in DMA transmissions
+#define USE_DMA
+
+/*
+ * use DMA-able memory for string storage
+ */
+char __attribute__((address(BANK1), coherent)) spi_buffer[] = "The quick brown fox jumps over the lazy dogs back";
+char __attribute__((address(BANK1 + 128), coherent)) spi_rec_buffer[128];
+volatile bool dmaT_done = false, dmaR_done = false, purge = true;
+
+/*
+ * DMA complete callbacks
+ */
+void SPI1DmaChannelHandler_State(DMAC_TRANSFER_EVENT, uintptr_t);
+void SPI2DmaChannelHandler_State(DMAC_TRANSFER_EVENT, uintptr_t);
+
+int main(void)
+{
+	uint32_t startCount, endCount;
+	/* Initialize all modules */
+	SYS_Initialize(NULL);
+
+	/* Start system tick timer */
+	CORETIMER_Start();
+
+	/*
+	 * SPI single W/R channel with split devices
+	 */
+	DMAC_ChannelCallbackRegister(DMAC_CHANNEL_0, SPI1DmaChannelHandler_State, 0); // TX, SCK pair
+	SPI1CONbits.STXISEL = DMA_GAP; // set to 0 for byte gaps
+	SPI1CONbits.ENHBUF = true; // enable FIFO
+
+	DMAC_ChannelCallbackRegister(DMAC_CHANNEL_7, SPI2DmaChannelHandler_State, 0); // RX, SCK pair
+	SS2_IN_InterruptDisable();
+
+	while (true) {
+#ifndef USE_DMA
+		LED_Toggle();
+#endif
+		CORETIMER_DelayMs(100); // 10 Hz updates for blink-led
+		dmaT_done = false;
+		dmaR_done = false;
+#ifdef USE_DMA
+		SPI2_REC_DATA_Set(); // debug sig
+		SS_CS_Clear(); // enable the remote slave SPI
+		DMAC_ChannelTransfer(DMAC_CHANNEL_7, (const void *) &SPI2BUF, (size_t) 1, (const void *) spi_rec_buffer, (size_t) strlen(spi_buffer), (size_t) 1);
+		RS_Set(); // debug sig
+		DMAC_ChannelTransfer(DMAC_CHANNEL_0, (const void *) spi_buffer, (size_t) strlen(spi_buffer), (const void *) &SPI1BUF, (size_t) 1, (size_t) 1);
+		/* Calculate the end count for the given delay */
+		endCount = (CORE_TIMER_FREQUENCY / 1000000) * 200;
+		startCount = _CP0_GET_COUNT();
+		while (!dmaR_done) { // While DMA running processing loop, check for receive errors or timeouts
+			CSB_Toggle(); // do something
+			if (purge) { // clear out startup SPI2 receiver buffer junk
+				purge = false; // run only once
+				break;
+			}
+
+			if ((_CP0_GET_COUNT() - startCount) > endCount) { // timeout after 200us
+				LED_Toggle(); // blink per block received
+				SS_CS_Set(); // disable slave SPI
+				SPI2_REC_DATA_Clear();
+				break;
+			};
+		};
+#else
+		SPI1_Write(spi_buffer, strlen(spi_buffer));
+#endif
+	}
+	/* Execution should not come here during normal operation */
+	return( EXIT_FAILURE);
+}
+
+/*
+ * interrupt at the end of strlen(spi_buffer) DMA byte TX transfers
+ */
+void SPI1DmaChannelHandler_State(DMAC_TRANSFER_EVENT event, uintptr_t contextHandle)
+{
+	if (event == DMAC_TRANSFER_EVENT_COMPLETE) {
+		dmaT_done = true;
+		RS_Clear();
+	}
+}
+
+/*
+ * interrupt at the end of receive DMA strlen(spi_buffer) RX byte transfer
+ */
+void SPI2DmaChannelHandler_State(DMAC_TRANSFER_EVENT event, uintptr_t contextHandle)
+{
+	if (event == DMAC_TRANSFER_EVENT_COMPLETE) {
+		dmaR_done = true;
+		LED_Toggle(); // blink per block received
+		SS_CS_Set(); // disable slave SPI
+		SPI2_REC_DATA_Clear();
+	}
+
+}
+
